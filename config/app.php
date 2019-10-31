c3fccf156837002b9f622026ff7940ff837e072f
Andre <dredix84@gmail.com>
2019-10-30 21:25:46 -0400

    Sending error logs to SysLog

--- config/app.php
+++ config/app.php
@@ -341,12 +341,13 @@ return [
             'levels'    => ['notice', 'info', 'debug'],
         ],
         'error'   => [
-            'className' => FileLog::class,
-            'path'      => LOGS,
-            'file'      => 'error',
-            'url'       => env('LOG_ERROR_URL', null),
-            'scopes'    => false,
-            'levels'    => ['warning', 'error', 'critical', 'alert', 'emergency'],
+//            'className' => FileLog::class,
+//            'path'      => LOGS,
+//            'file'      => 'error',
+//            'url'       => env('LOG_ERROR_URL', null),
+//            'scopes'    => false,
+//            'levels'    => ['warning', 'error', 'critical', 'alert', 'emergency'],
+            'engine' => 'Syslog'
         ],
         // To enable this dedicated query log, you need set your datasource's log flag to true
         'queries' => [
